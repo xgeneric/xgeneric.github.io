bc6f61ce332b901c8fecee502f5fe891eb4e0a24
xgeneric <168141409+xgeneric@users.noreply.github.com>
2024-10-20 18:32:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const path = require("path");
 const request = require("request-promise");
+
 app.use(express.json());
-request.get("https://goldquest.blooket.com/gold/play/landing",function(a,resp,c){
-global.cookiew = resp.headers["set-cookie"][0].split(";")[0];
-console.log("Blooket Cookie Ready!");
+
+request.get("https://goldquest.blooket.com/gold/play/landing", function(a, resp, c) {
+  global.cookiew = resp.headers["set-cookie"][0].split(";")[0];
+  console.log("Blooket Cookie Ready!");
 });
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "/rojolet.html"));
 });
+
 app.get('/script.js', (req, res) => {
   res.sendFile(path.join(__dirname, "/script.js"));
 });
-app.post('/join', (req, res) => {
+
+app.get('/join', (req, res) => {
   try {
-  request.put({body:req.body,json:true,uri:"https://fb.blooket.com/c/firebase/join",headers:{Cookie:global.cookiew}}).pipe(res);
-  } catch(e){res.send(JSON.stringify({success:false,errType:"",msg:e}));}
-  console.log("Joining game " + req.body.id + " with name " + req.body.name + "!");
+    const { id, name } = req.query;
+    if (!id || !name) {
+      return res.status(400).send(JSON.stringify({ success: false, msg: "Missing game id or name" }));
+    }
+
+    request.put({
+      body: { id, name },
+      json: true,
+      uri: "https://fb.blooket.com/c/firebase/join",
+      headers: { Cookie: global.cookiew }
+    }).pipe(res);
+    
+    console.log("Joining game " + id + " with name " + name + "!");
+  } catch (e) {
+    res.send(JSON.stringify({ success: false, errType: "", msg: e }));
+  }
 });
+
 app.listen(process.env.PORT, function() {
   console.log('Webserver started on port ' + process.env.PORT + '!');
 });
