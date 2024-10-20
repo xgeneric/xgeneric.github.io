53d05b57bba04c4c531c31c6ff347845fdd104f7
xgeneric <168141409+xgeneric@users.noreply.github.com>
2024-10-20 18:24:02 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const path = require("path");
+const request = require("request-promise");
+app.use(express.json());
+request.get("https://goldquest.blooket.com/gold/play/landing",function(a,resp,c){
+global.cookiew = resp.headers["set-cookie"][0].split(";")[0];
+console.log("Blooket Cookie Ready!");
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+app.get('/script.js', (req, res) => {
+  res.sendFile(path.join(__dirname, "/script.js"));
+});
+app.post('/join', (req, res) => {
+  try {
+  request.put({body:req.body,json:true,uri:"https://fb.blooket.com/c/firebase/join",headers:{Cookie:global.cookiew}}).pipe(res);
+  } catch(e){res.send(JSON.stringify({success:false,errType:"",msg:e}));}
+  console.log("Joining game " + req.body.id + " with name " + req.body.name + "!");
+});
+app.listen(process.env.PORT, function() {
+  console.log('Webserver started on port ' + process.env.PORT + '!');
+});
