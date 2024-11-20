bdbfe2ac261d5a6099ed142bf3f96f6449f2636c
xgeneric <168141409+xgeneric@users.noreply.github.com>
2024-11-19 21:40:26 -0500

    Add files via upload

---
+++ static/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/static/load/',
+    bare:'https://static.99.135.156.178.clients.your-server.de/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
